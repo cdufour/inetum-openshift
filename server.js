98ab67c56dcf8e5e670a772ec55507f3e6e0a304
Opusidea <opusidea@gmail.com>
2022-05-02 15:42:15 +0200

    Add square route

--- server.js
+++ server.js
@@ -7,4 +7,9 @@ app.get('/', (req, res) => {
   res.send('ok');
 });
 
+app.get('/square/:n', (req, res) => {
+  const n = parseInt(req.params.n);
+  res.send(`Le carré de ${n} vaut ${n*n}`);
+});
+
 app.listen(port, () => console.log('Serveur écoutant le port ' + port));
