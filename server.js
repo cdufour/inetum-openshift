45b1010f47da10c023c5360fdb13c549b5889e98
Opusidea <opusidea@gmail.com>
2022-05-03 12:42:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,27 +2,27 @@ const express = require('express');
 
 const mariadb = require('mariadb');
 
-try {
-  
-  console.log(
-    process.env.DB_HOST, 
-    process.env.DB_USER, 
-    process.env.DB_NAME,
-    process.env.DB_PASSWORD
-  );
-  
-  const pool = mariadb.createPool({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USER, 
-    connectionLimit: 5
-  });
-  
-  
-  console.log("MARIADB OK");
-  
-} catch(e) {
-  console.log("MARIADB NOT OK");
-  console.log(e);
+const pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASSWORD,
+  connectionLimit: 5
+});
+
+async function asyncFunction() {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query("SELECT 1 as val");
+    console.log(rows); //[ {val: 1}, meta: ... ]
+    const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+    console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+  } catch (err) {
+	  throw err;
+  } finally {
+	  if (conn) return conn.end();
+  }
 }
 
 const app = express();
